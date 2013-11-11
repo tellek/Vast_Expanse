2334791b4af5d25202aff5f4bbd2cb42625c3542
Topher <TopherBirth@Gmail.com>
2013-11-10 20:23:58 -0600

    Added Trees and some Citizen behavior

---
+++ Assets/Scripts/SelectionSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionSystem : MonoBehaviour {
+
+	public string Current_Selection = "Nothing";
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray, out hit);
+			Debug.Log("Current Selection: " + hit.transform.name); ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(hit.transform.name != "Plane")
+			{
+				Current_Selection = hit.transform.name;
+			}
+		}
+
+		
+	}
+	
+	public GUISkin customSkin;
+	
+	void OnGUI(){
+		GUI.skin = customSkin;
+		GUI.skin.label.alignment = TextAnchor.MiddleLeft;
+		GUI.Box (new Rect(10,10,428,23),"Right-click to move, Hold the Right Mouse Button to continuously move.");
+		GUI.Box (new Rect(10,35,250,23),"Current Selection: " + Current_Selection);
+	}
+}
---
+++ Assets/Scripts/Worker.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Worker : MonoBehaviour {
+	
+	public bool busy, woodCutter, miner, builder;
+	public float woodCutterExp, minerExp, builderExp, health, hunger, energy;
+	public List<Vector3> actionList;
+	public float speed = 5f;
+	public float turnSpeed = 10f;
+	
+	private Vector3 targetPosition;
+	private GameObject targetObject;
+	private Transform targetTransform;
+	private bool working;
+	private float workTime;
+
+	void Start() {
+		targetPosition = transform.position;
+		woodCutter = true;
+	}
+	
+	//
+	private TreeController GenTest;
+	//
+	
+	void Update () {
+		if(transform.position != targetPosition)
+		{
+			MoveTo(targetPosition);
+		}
+		
+		if(transform.position == targetPosition && working == true)
+		{
+			Debug.Log("Worker: Starting task.");
+			StartCoroutine(BeginWork(workTime));
+			working = false;
+		}
+		
+		if (woodCutter == true && busy == false)
+		{
+			busy = true;
+			targetObject = FindTree();
+			//
+			GenTest = targetObject.GetComponent<TreeController>();
+			GenTest.claimed = true;
+			//
+			targetTransform = targetObject.transform;
+			targetPosition = targetObject.transform.position;
+			workTime = 5f; //Adjusted later based off multiple values.
+			working = true;
+		}
+	}
+	
+	//MOVEMENT AND ROTATION
+	void MoveTo(Vector3 target)
+	{
+		Quaternion _lookRotation;
+		Vector3 _direction;
+		
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		
+		_direction = (targetTransform.position - transform.position).normalized;
+		_lookRotation = Quaternion.LookRotation(_direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * turnSpeed);
+	}
+	
+	GameObject FindTree()
+	{
+		GameObject[] treeLocations = GameObject.FindGameObjectsWithTag("Tree");
+		float distance = float.MaxValue;
+		GameObject closestTree = GameObject.FindGameObjectWithTag("Tree");
+		
+		foreach (GameObject aTree in treeLocations)
+		{
+			Vector3 difference = aTree.transform.position - transform.position;
+			float thisDistance = difference.sqrMagnitude;
+			//
+			GenTest = aTree.GetComponent<TreeController>();
+			//
+			if(distance > thisDistance && GenTest.claimed == false)
+			{
+				closestTree = aTree;
+				distance = thisDistance;
+			}
+		}
+		Debug.Log("Worker: Closest Tree: " + closestTree.name + closestTree.transform.position);
+		return closestTree;
+	}
+	
+	IEnumerator BeginWork(float seconds)
+	{
+		Debug.Log("Worker: Choopping wood.");
+		yield return new WaitForSeconds (seconds);
+		Object.Destroy(targetObject);
+		busy = false;
+		
+	}
+	
+	
+}
